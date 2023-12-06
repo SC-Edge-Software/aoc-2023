64b8db4baf5341e3e6dac875a398621063476a38
Sam Claflin <sam.claflin@edgesoftware.dev>
2023-12-05 19:50:55 -0500

    Created template files for Day 6

---
+++ day_6/part_1.py
@@ -0,0 +1,11 @@
+INPUT_FILE = "input.txt"
+
+
+def main():
+    with open(INPUT_FILE, "r") as f:
+        # TODO
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
---
+++ day_6/part_2.py
@@ -0,0 +1,11 @@
+from part_1 import INPUT_FILE
+
+
+def main():
+    with open(INPUT_FILE, "r") as f:
+        # TODO
+        f.close()
+
+
+if __name__ == "__main__":
+    main()
